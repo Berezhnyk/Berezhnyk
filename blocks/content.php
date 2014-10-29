6049bacb55dec488434756117198a7423af8b3e1
Ivan <ivanberezhnyk@gmail.com>
2014-10-29 18:06:19 +0200

    дло
    дтд

--- blocks/content.php
+++ blocks/content.php
@@ -12,16 +12,16 @@
 echo '<table width="100%" style="padding-top:20px;">';
 if(!isset($_GET['p'])) $_GET['p'] =0;
 $p = $_GET['p'];
-$per_page = 10; // кількість виводимого товару нв сторінці
+$per_page = 10;
 if(isset($_GET['id_cat'])){
 	$sql_num = "SELECT id_good FROM goods WHERE id_cat='$id_cat'";
 } else {
 	$sql_num = "SELECT id_good FROM goods";
 }
 $query_num = mysql_query($sql_num) or die(mysql_error());
-// загальна кількість товару
+// Р·Р°РіР°Р»СЊРЅР° Рєв‰ҐР»СЊРєв‰ҐСЃС‚СЊ С‚РѕРІР°СЂСѓ
 $rows = mysql_num_rows($query_num);
-// кількість сторінок
+// Рєв‰ҐР»СЊРєв‰ҐСЃС‚СЊ СЃС‚РѕСЂв‰ҐРЅРѕРє
 $pages = ceil($rows/$per_page);
 if($id_cat >= 0){
 	$sql_good="SELECT * FROM goods WHERE id_cat='$id_cat' ORDER BY id_good ASC LIMIT ".$_GET['p'].",".$per_page;
@@ -35,7 +35,7 @@ while($row_good = mysql_fetch_assoc($query_good)){
 	$str = preg_replace('/([^\s]{20})[^\s]+/', '<span title="$0">$1...</span>', $row_good['name_good']);
 	?>	
 		<table>
-		<tr><td style="width:500px;"><a href="goods.php?id_good=<?=$row_good['id_good']?>&id_cat=<?=$id_cat?>&p=<?=$p?>"><?=$str;?></a></td><td><?=$row_good['price_good']?> грн.</td><td><a class="button blue" style="width:50px; height:25px;" href="add_basket.php?id_good=<?=$row_good['id_good']?>&id_cat=<?=$id_cat ?>&p=<?=$_GET['p']?>">в корзину</a></td></tr>
+		<tr><td style="width:500px;"><a href="goods.php?id_good=<?=$row_good['id_good']?>&id_cat=<?=$id_cat?>&p=<?=$p?>"><?=$str;?></a></td><td><?=$row_good['price_good']?> РіСЂРЅ.</td><td><a class="button blue" style="width:50px; height:25px;" href="add_basket.php?id_good=<?=$row_good['id_good']?>&id_cat=<?=$id_cat ?>&p=<?=$_GET['p']?>">РІ РєРѕСЂР·РёРЅСѓ</a></td></tr>
 		
         </table>
      <?php
