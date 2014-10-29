6c40231102060ae8b6fe7f293ee12c7d5b205f1e
Ivan <ivanberezhnyk@gmail.com>
2014-10-29 20:01:53 +0200

    Перегляди

--- goods.php
+++ goods.php
@@ -4,14 +4,6 @@ $session_id = session_id();
 <?php  include_once 'admin/blocks/bd.php'; ?>
 <?php require 'blocks/sum.php';?>
 <?php include 'blocks/header.php';?>
-<!--
-if (!isset($_COOKIE['view_'.$id])) {
-setcookie("view_".$id, 1, time()+3600);
-mysql_query("UPDATE 2_view SET
-col_prosmot=col_prosmot+1 WHERE id='$id'");
-}
--->
-
 <p align="left">
 <input class="button gray" type="button" onclick="history.back()" value="<== Назад">
 </p>
@@ -21,16 +13,23 @@ col_prosmot=col_prosmot+1 WHERE id='$id'");
 </div><!--корзина-->
 <?php include 'blocks/begintable.php';?>
 Про товар<br>
-Переглядів: <!--echo $col_prosmot; -->
+
 <?php
 if(isset($_GET)){
 $id_good = $_GET['id_good'];
+
+if (!isset($_COOKIE['view_'.$id_good])) {
+    setcookie("view_".$id_good, 1, time()+3600);
+    mysql_query("UPDATE goods SET views = views+1 WHERE id_good='$id_good'");
+}
+
 $id_cat = $_GET['id_cat'];
 $p = $_GET['p'];
 
 $sql_good ="SELECT * FROM goods WHERE id_good='$id_good' ORDER BY id_good ASC";
 $query_good = mysql_query($sql_good) or die(mysql_error());
 $row_good = mysql_fetch_row($query_good);
+echo "Переглядів: ".$row_good[5];
 $str = preg_replace('/([^\s]{20})[^\s]+/', '<span title="$0">$1...</span>', $row_good[2]);
 echo '<h4 style="color:red;" align="center">'.$str.'</h4>';
 $str = preg_replace('/([^\s]{20})[^\s]+/', '<span title="$0">$1...</span>', $row_good[3]);
@@ -51,51 +50,9 @@ echo ('</div>');
 }
 
 ?>
-<div class="mypage" style="width:95%; background-color:#E9E9E9; font-family:'Times New Roman', Times, serif;">
 
-<form id="form1" name="form1" action="add_comments_save.php"  method="post">
-		<table>
-		<tr>
-		<td align="right">
-        <label>Ваш нікнейм</label><br/>
-		</td>
-		<td align="left">
-        <input name="nic" type="text"></textarea><br/>
-		</td>
-		</tr>
-		<tr>
-		<td align="right">
-        <label>Ваше враження про товар</label>
-		</td>
-		<td align="left">
-        <textarea name="comment" id="comment" cols="45" rows="5"><?=$row_good['info']?></textarea>
-		</td>
-		</tr>
-        <input name="id_good" type="hidden" value="<?=$id_good?>">
-		<tr>
-		<td align="right">
-        <input name="submit" type="submit" value="Надіслати"><br/>
-		</td>
-		</tr>
-        </table>
-        </form>
-		</div>
-<?php
-$sql_comm ="SELECT * FROM comments WHERE id_good='$id_good' ORDER BY id_comm DESC";
-$query_comm = mysql_query($sql_comm) or die(mysql_error());
-while($row_comm = mysql_fetch_assoc($query_comm)){
-	?>
-	<div class="mypage" style="width:95%; background-color:#E9E9E9; font-family:'Times New Roman', Times, serif;">
+
 	<?php
-			$str = $row_comm['nic'];
-			echo '<li style="list-style-type: none;">'.$str.'</a></li>';
-			$str = $row_comm['comment'];
-echo '<li class="mypage" style="list-style-type: none; width:95%; background-color:#FDFDFD;">'.$str.'</li>';
-			
-	?>
-	</div>
-	<?php
-		}
 
 require 'blocks/footer.php';
 include 'blocks/endtable.php';?>
